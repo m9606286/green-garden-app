0ae02221875abb233360e65d38a516bdb4de9d03
m9606286 <83000261+m9606286@users.noreply.github.com>
2025-11-07 13:32:59 +0800

    Update app.py

--- my_app/app.py
+++ my_app/app.py
@@ -705,8 +705,8 @@ def main():
                     else:
                         st.info("資料未修改，無需更新")
                     # form 區塊結束後才 rerun
-                if rerun_flag:
-                    st.experimental_rerun()
+                    if rerun_flag:
+                        st.experimental_rerun()
                 
     with tab2:
         # 產品選擇
@@ -944,6 +944,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
